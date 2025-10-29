db04a0c06f3ad24561813ba362493aa09b37fee2
Zsar <lordzsar1@aol.com>
2025-10-29 17:20:47 +0100

    make multi-barrel guns shoot all barrels simultaneously

--- data/scripts/ZsarVanillaTweaksModPlugin.java
+++ data/scripts/ZsarVanillaTweaksModPlugin.java
@@ -6,11 +6,16 @@ import com.fs.starfarer.api.campaign.GenericPluginManagerAPI;
 import com.fs.starfarer.api.campaign.SectorAPI;
 import com.fs.starfarer.api.campaign.SectorEntityToken;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.combat.WeaponAPI;
 import com.fs.starfarer.api.impl.campaign.procgen.themes.PKDefenderPluginImpl;
+import com.fs.starfarer.api.loading.WeaponSpecAPI;
+import com.fs.starfarer.loading.specs.N;
 
 import com.thoughtworks.xstream.XStream;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
 
 @SuppressWarnings("unused") // dynamically loaded
 public class ZsarVanillaTweaksModPlugin extends BaseModPlugin {
@@ -23,6 +28,35 @@ public class ZsarVanillaTweaksModPlugin extends BaseModPlugin {
 	@Override
 	public void configureXStream(XStream x) {
 		x.alias("PKDefenderPluginImpl", PKDefenderPlugin.class);
+	}
+
+	@Override
+	public void onApplicationLoad() {
+		try {
+			final N.o barrelMode = N.o.valueOf("LINKED");
+			for (final WeaponSpecAPI weaponSpec : Global.getSettings().getAllWeaponSpecs()) {
+				final int countBarrels = weaponSpec.getTurretAngleOffsets().size();
+				if ( 1 < countBarrels
+				 &&  WeaponAPI.WeaponType.MISSILE != weaponSpec.getType()
+				 && !weaponSpec.getAIHints().contains(WeaponAPI.AIHints.PD)
+				 && !"Spread Pattern".equals(weaponSpec.getAccuracyStr()) // looks horrible
+				 &&  weaponSpec instanceof N
+				 &&  barrelMode != ((N) weaponSpec).getMode()) {
+					final N spec = (N) weaponSpec;
+					spec.setMode(barrelMode);
+					if (1 == spec.getBurstSize()) // e.g. Hephaestus Assault Gun - no burst size to decrease here
+						spec.setRefireDelay(spec.getRefireDelay() * countBarrels);
+					else                          // e.g. Heavy Autocannon - game multiplies burst size with barrel count on BarrelMode.LINKED, so we need to de-multiply it again
+						spec.setBurstSize((int) Math.ceil((float) spec.getBurstSize() / (float) countBarrels));
+				}
+			}
+		}
+		catch (final Throwable e) {
+			final ArrayList<String> barrelModeNames = new ArrayList<String>(N.o.values().length);
+			for (final N.o barrelMode : N.o.values())
+				barrelModeNames.add(barrelMode.name());
+			this.log.error(String.format("Expected to find Barrel Mode 'LINKED' but only found %s", barrelModeNames), e);
+		}
 	}
 
 	@Override
