41374370edbe82dc8ed1666484c7b01ff42e9177
Zsar <lordzsar1@aol.com>
2025-10-29 18:14:33 +0100

    convert to .jar mod

--- src/ZsarVanillaTweaksModPlugin.java
+++ src/ZsarVanillaTweaksModPlugin.java
@@ -1,9 +1,5 @@
-package data.scripts;
-
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.GenericPluginManagerAPI;
-import com.fs.starfarer.api.campaign.SectorAPI;
 import com.fs.starfarer.api.campaign.SectorEntityToken;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.combat.WeaponAPI;
@@ -15,7 +11,9 @@ import com.thoughtworks.xstream.XStream;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+
+import zsar.vanillatweaks.PKDefenderPlugin;
 
 @SuppressWarnings("unused") // dynamically loaded
 public class ZsarVanillaTweaksModPlugin extends BaseModPlugin {
@@ -33,17 +31,17 @@ public class ZsarVanillaTweaksModPlugin extends BaseModPlugin {
 	@Override
 	public void onApplicationLoad() {
 		try {
-			final N.o barrelMode = N.o.valueOf("LINKED");
+			final var barrelMode = N.o.valueOf("LINKED");
 			for (final WeaponSpecAPI weaponSpec : Global.getSettings().getAllWeaponSpecs()) {
-				final int countBarrels = weaponSpec.getTurretAngleOffsets().size();
+				final var countBarrels = weaponSpec.getTurretAngleOffsets().size();
 				if ( 1 < countBarrels
 				 &&  WeaponAPI.WeaponType.MISSILE != weaponSpec.getType()
 				 && !weaponSpec.getAIHints().contains(WeaponAPI.AIHints.PD)
 				 && !"Spread Pattern".equals(weaponSpec.getAccuracyStr()) // looks horrible
-				 &&  weaponSpec instanceof N
-				 &&  barrelMode != ((N) weaponSpec).getMode()) {
-					final N spec = (N) weaponSpec;
+				 &&  weaponSpec instanceof N spec
+				 &&  barrelMode != spec.getMode()) {
 					spec.setMode(barrelMode);
+					//spec.setSeparateRecoilForLinkedBarrels(true);
 					if (1 == spec.getBurstSize()) // e.g. Hephaestus Assault Gun - no burst size to decrease here
 						spec.setRefireDelay(spec.getRefireDelay() * countBarrels);
 					else                          // e.g. Heavy Autocannon - game multiplies burst size with barrel count on BarrelMode.LINKED, so we need to de-multiply it again
@@ -52,34 +50,29 @@ public class ZsarVanillaTweaksModPlugin extends BaseModPlugin {
 			}
 		}
 		catch (final Throwable e) {
-			final ArrayList<String> barrelModeNames = new ArrayList<String>(N.o.values().length);
-			for (final N.o barrelMode : N.o.values())
-				barrelModeNames.add(barrelMode.name());
+			final var barrelModeNames = Arrays.stream(N.o.values()).map(Enum::name).toList();
 			this.log.error(String.format("Expected to find Barrel Mode 'LINKED' but only found %s", barrelModeNames), e);
 		}
 	}
 
 	@Override
 	public void onGameLoad(boolean newGame) {
-		final GenericPluginManagerAPI plugins = Global.getSector().getGenericPlugins();
-		// Janino cannot Method References ~~ -Zsar 2025-10-29
-		for (final GenericPluginManagerAPI.GenericPlugin plugin : plugins.getPluginsOfClass(PKDefenderPluginImpl.class))
-			plugins.removePlugin(plugin);
+		final var plugins = Global.getSector().getGenericPlugins();
+		plugins.getPluginsOfClass(PKDefenderPluginImpl.class).forEach(plugins::removePlugin);
 		plugins.addPlugin(new PKDefenderPlugin(), true);
 	}
 
 	@Override
 	public void onNewGame() {
-		final SectorAPI sector = Global.getSector();
-		final MarketAPI market = sector.getEconomy().getMarket("derinkuyu_market");
-		final SectorEntityToken station = sector.getEntityById("derinkuyu_station");
-		this.shortenNameOfDerinkuyuStation(market, station);
+		final var sector = Global.getSector();
+		this.shortenNameOfDerinkuyuStation(sector.getEconomy().getMarket("derinkuyu_market"),
+		                                   sector.getEntityById("derinkuyu_station"));
 	}
 
 	/** In-game the station is consistently referred to as "Derinkuyu Station", only the UI calls it "Derinkuyu Mining Station". */
 	private void shortenNameOfDerinkuyuStation(final MarketAPI market, final SectorEntityToken station) {
-		final String shortName = "Derinkuyu Station";
-		final String wastefulQualifier = " Mining ";
+		final var shortName = "Derinkuyu Station";
+		final var wastefulQualifier = " Mining ";
 		if (market.getName().contains(wastefulQualifier)) {
 			market.setName(shortName);
 		}
