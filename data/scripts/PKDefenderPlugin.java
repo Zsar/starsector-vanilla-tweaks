e082ee92f66ea19aa7a538e24da293cc7279b9b8
Zsar <lordzsar1@aol.com>
2025-10-29 05:31:27 +0100

    fix 'Remnants of Task Force Safeguard' behaving differently from other unique ships
    ? tags on fleet 'Remnants of Task Force Safeguard'
    	+ VARIANT_ALWAYS_RECOVERABLE
    	- TAG_RETAIN_SMODS_ON_RECOVERY
    	- VARIANT_UNRESTORABLE

---
+++ data/scripts/PKDefenderPlugin.java
@@ -0,0 +1,22 @@
+package data.scripts;
+
+import com.fs.starfarer.api.campaign.CampaignFleetAPI;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import com.fs.starfarer.api.impl.campaign.ids.Tags;
+import com.fs.starfarer.api.impl.campaign.procgen.themes.PKDefenderPluginImpl;
+import com.fs.starfarer.api.impl.campaign.rulecmd.salvage.SalvageGenFromSeed;
+
+import java.util.Random;
+
+public class PKDefenderPlugin extends PKDefenderPluginImpl {
+	@Override
+	public void modifyFleet(SalvageGenFromSeed.SDMParams p, CampaignFleetAPI fleet, Random random, boolean withOverride) {
+		super.modifyFleet(p, fleet, random, withOverride);
+
+		for (final FleetMemberAPI curr : fleet.getFleetData().getMembersListCopy()) {
+			curr.getVariant().addTag(Tags.VARIANT_ALWAYS_RECOVERABLE);
+			curr.getVariant().removeTag(Tags.TAG_RETAIN_SMODS_ON_RECOVERY);
+			curr.getVariant().removeTag(Tags.VARIANT_UNRESTORABLE);
+		}
+	}
+}
