e998d5fb675be30f4cc3b65ebc8a80a8c25520b2
Zsar <lordzsar1@aol.com>
2026-05-13 05:35:57 +0200

    (amend) alternative implementation by commodity production instead of mere industry presence

--- src/zsar/vanillatweaks/BlackMarketPlugin.java
+++ src/zsar/vanillatweaks/BlackMarketPlugin.java
@@ -1,116 +1,65 @@
 package zsar.vanillatweaks;
 
-import com.fs.starfarer.api.campaign.econ.Industry;
+import com.fs.starfarer.api.impl.campaign.econ.CommodityIconCounts;
 import com.fs.starfarer.api.impl.campaign.ids.Commodities;
 import com.fs.starfarer.api.impl.campaign.ids.Industries;
 import com.fs.starfarer.api.util.Misc;
 import com.fs.starfarer.api.util.WeightedRandomPicker;
 
 public class BlackMarketPlugin extends com.fs.starfarer.api.impl.campaign.submarkets.BlackMarketPlugin {
-	public static float pOther = .1f;
-
-	public enum Proliferation {
-		NONE(0.f),
-		SOME(5.f),
-		MUCH(2.f * SOME.value)
-		;
-		public final float value;
-
-		Proliferation(final float value) { this.value = value; }
-
-		public static Proliferation max(final Proliferation a, final Proliferation b) {
-			return Math.max(a.value, b.value) == a.value ? a : b;
-		}
-	}
-
-	public static Proliferation fromGoodConsumption(final Proliferation soFar, final Industry consumer) {
-		return Proliferation.max(!consumer.isBuilding() || consumer.isUpgrading()
-		                         	? Proliferation.SOME : Proliferation.NONE,
-		                         soFar);
-	}
-
-	public static Proliferation fromGoodProduction(final Proliferation soFar, final Industry producer) {
-		return Proliferation.max(producer.isBuilding() && !producer.isUpgrading()
-		                         	? Proliferation.NONE
-		                         	: null == producer.getSpecialItem()
-		                         	? Proliferation.SOME
-		                         	: Proliferation.MUCH,
-		                         soFar);
-	}
-
-	public static Proliferation fromShipProduction(final Proliferation soFar, final Industry producer) {
-		return Proliferation.MUCH == soFar || producer.isFunctional()
-		     ? Proliferation.MUCH : Proliferation.NONE;
-	}
-
-	/** If the industry that nominally needs these ships is disrupted, increase Black Market supply by one step. */
-	public static Proliferation blackMarketSpecial(final Proliferation soFar, final Industry producer) {
-		return Proliferation.MUCH == soFar || !producer.isDisrupted()
-		     ? soFar
-		     : Proliferation.SOME == fromGoodProduction(soFar, producer)
-		     ? Proliferation.MUCH
-		     : Proliferation.SOME;
-	}
-
-	public static boolean demands(final Industry industry, final String commodity) {
-		return industry.getDemand(commodity).getQuantity().isPositive();
-	}
-
-	public static boolean supplies(final Industry industry, final String commodity) {
-		return industry.getSupply(commodity).getQuantity().isPositive();
-	}
-
-	public float fraction(final Proliferation ofRelevantCommodity) {
-		return (super.itemGenRandom.nextFloat() <= pOther ? Proliferation.MUCH : ofRelevantCommodity).value;
-	}
-
 	public static float fractionCombatShips(final boolean military) {
-		return military ? 70.f : 10.f;
+		return military ? 70.f : 0.f;
 	}
 
-	public float[] fractionsNonCombatShips() {
-		var goodProductionFreighter = Proliferation.NONE;
-		var goodProductionTanker    = Proliferation.NONE;
-		var goodProductionTransport = Proliferation.NONE; // Note: specifically *Troop* Transports
-		var goodProductionLiner     = super.market.getSize() > 6 // at least 10'000'000 inhabitants
-		                            ? Proliferation.MUCH
-		                            : super.market.getSize() > 3 // at least     10'000 inhabitants
-		                            ? Proliferation.SOME
-		                            : Proliferation.NONE;
-		var goodProductionUtility   = Proliferation.NONE; // Note: defined in data/world/factions/default_ship_roles.json as "utility"
-		var shipProduction = Proliferation.NONE;
-		for (final var industry : super.market.getIndustries()) {
-			if (industry.isIndustry())
-				goodProductionFreighter = fromGoodConsumption(goodProductionFreighter, industry);
-			if (industry.isIndustry() || Industries.MEGAPORT.equals(industry.getId()))
-				goodProductionFreighter = blackMarketSpecial(goodProductionFreighter, industry);
-			if (demands(industry , Commodities.FUEL))
-				goodProductionTanker = fromGoodConsumption(goodProductionTanker, industry);
-			if (supplies(industry, Commodities.FUEL))
-				goodProductionTanker = blackMarketSpecial(goodProductionTanker, industry);
-			if (demands(industry , Commodities.MARINES))
-				goodProductionTransport = fromGoodConsumption(goodProductionTransport, industry);
-			if (supplies(industry, Commodities.MARINES))
-				goodProductionTransport = blackMarketSpecial(goodProductionTransport, industry);
-			if (Industries.TECHMINING.equals(industry.getId()))
-				goodProductionUtility = blackMarketSpecial(goodProductionUtility, industry);
-			if (supplies(industry, Commodities.SHIPS))
-				shipProduction = fromShipProduction(shipProduction, industry);
+	public float[] fractions() {
+		var goodProductionCombat    = 0;
+		var goodProductionFreighter = 0;
+		var goodProductionTanker    = 0;
+		var goodProductionTransport = 0; // Note: specifically *Troop* Transports
+		var goodProductionLiner     = super.market.getSize() - 1;
+		var goodProductionUtility   = 0; // Note: defined in data/world/factions/default_ship_roles.json as "utility"
+		var commodities = super.market.getAllCommodities();
+		for (var commodity : commodities) {
+			final var id = commodity.getId();
+			final var data = new CommodityIconCounts(commodity);
+			final var exportTargetCurrent = data.production - data.demandMetWithLocal;
+			final var importTargetCurrent = data.demand     - data.demandMetWithLocal;
+			// how much of this commodity travels through space, in 10^x units
+			final var throughputCurrent = Math.max(0, Math.max(exportTargetCurrent, importTargetCurrent));
+			final var throughputMaximal = Math.abs(commodity.getMaxDemand() - commodity.getMaxSupply());
+			if (throughputMaximal > 0) {
+				// one ship per unit, and one ship again per unit disrupted, to account for "stranded" truckers waiting for goods
+				final var fromTrade = 2 * throughputMaximal - throughputCurrent;
+				if (!(commodity.isFuel() || commodity.isPersonnel() || commodity.isMeta() || commodity.isNonEcon()))
+					goodProductionFreighter += fromTrade;
+				else if (commodity.isFuel())
+					goodProductionTanker    += fromTrade;
+				if (commodity.getCommodity().hasTag(Commodities.MARINES))
+					goodProductionTransport += fromTrade;
+				if (Commodities.SHIPS.equals(commodity.getId())) {
+					goodProductionCombat    += fromTrade;
+					goodProductionFreighter += fromTrade;
+					goodProductionTanker    += fromTrade;
+					goodProductionTransport += fromTrade;
+					goodProductionLiner     += fromTrade;
+					goodProductionUtility   += fromTrade;
+				}
+			}
 		}
-		return Proliferation.NONE != shipProduction ? new float[] {
-		                                              	shipProduction.value,
-		                                              	shipProduction.value,
-		                                              	shipProduction.value,
-		                                              	shipProduction.value,
-		                                              	shipProduction.value,
-		                                              }
-		                                            : new float[] {
-		                                              	this.fraction(goodProductionFreighter),
-		                                              	this.fraction(goodProductionTanker),
-		                                              	this.fraction(goodProductionTransport),
-		                                              	this.fraction(goodProductionLiner),
-		                                              	this.fraction(goodProductionUtility),
-		                                              };
+		if (super.market.getIndustries().stream().anyMatch(industry -> {
+			var id = industry.getId();
+			return industry.getSpec().hasTag(Industries.TAG_BATTLESTATION) && !industry.isDisrupted() // feels odd to expect them on the market, while the station is disrupted
+			    || Industries.TECHMINING.equals(id) || Industries.WAYSTATION.equals(id);              // - one would think they're out and about helping to clean up
+		}))
+			goodProductionUtility += Math.max(1, super.market.getSize() - 2); // at least one, but overall rather few
+		return new float[] {
+			super.itemGenRandom.nextFloat() * goodProductionCombat,
+			super.itemGenRandom.nextFloat() * goodProductionFreighter,
+			super.itemGenRandom.nextFloat() * goodProductionTanker,
+			super.itemGenRandom.nextFloat() * goodProductionTransport,
+			super.itemGenRandom.nextFloat() * goodProductionLiner,
+			super.itemGenRandom.nextFloat() * goodProductionUtility,
+		};
 	}
 
 	@Override
@@ -144,14 +93,14 @@ public class BlackMarketPlugin extends com.fs.starfarer.api.impl.campaign.submar
 
 			final var doctrine = super.market.getFaction().getDoctrine().clone();
 			final var combatShipFraction = fractionCombatShips(military);
-			final var fractionsNonCombatShips = fractionsNonCombatShips();
+			final var fractions = fractions();
 			super.addShips(super.market.getFactionId(),
-				combatShipFraction,
-				fractionsNonCombatShips[0],
-				fractionsNonCombatShips[1],
-				fractionsNonCombatShips[2],
-				fractionsNonCombatShips[3],
-				fractionsNonCombatShips[4],
+				combatShipFraction + fractions[0],
+				fractions[1],
+				fractions[2],
+				fractions[3],
+				fractions[4],
+				fractions[5],
 				null,
 				0.f, // qualityMod
 				null,
@@ -163,12 +112,12 @@ public class BlackMarketPlugin extends com.fs.starfarer.api.impl.campaign.submar
 			doctrineOverride.setShipSize  (Math.max(doctrineOverride.getShipSize  (), doctrine.getShipSize()));
 			doctrineOverride.setCombatFreighterProbability(1.f);
 			super.addShips(super.submarket.getFaction().getId(),
-				combatShipFraction,
-				fractionsNonCombatShips[0],
-				fractionsNonCombatShips[1],
-				fractionsNonCombatShips[2],
-				fractionsNonCombatShips[3],
-				fractionsNonCombatShips[4],
+				combatShipFraction + fractions[0],
+				fractions[1],
+				fractions[2],
+				fractions[3],
+				fractions[4],
+				fractions[5],
 				Math.min(1f, Misc.getShipQuality(market, market.getFactionId()) + 0.5f),
 				0.f, // qualityMod
 				null,
